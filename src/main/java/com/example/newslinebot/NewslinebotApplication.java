9112aaac252c63bb4037774c6fc5046940976471
kyle <2305024iisi@group.com>
2025-12-03 17:47:32 +0800

    first commit

---
+++ src/main/java/com/example/newslinebot/NewslinebotApplication.java
@@ -0,0 +1,13 @@
+package com.example.newslinebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewslinebotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewslinebotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/newslinebot/NewslinebotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newslinebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewslinebotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
